0926e61909acbfc1ee35c540becfbfc0644a24e7
abhanani <42608100+abhanani@users.noreply.github.com>
2019-05-05 18:21:40 +0530

    Create JenkinsPipelineVars.groovy

---
+++ vars/JenkinsPipelineVars.groovy
@@ -0,0 +1,6 @@
+def call(){
+    echo '*****************************************************************************'
+    echo '** This pipeline step (deployToWholeDatacenterWithRollback) is deprecated! **'
+    echo '** Please replace with deployTagToDatacenter()                             **'
+    echo '*****************************************************************************'
+}
