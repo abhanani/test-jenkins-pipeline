2f82afedadf1bafdee63feb631578a4ecceab5d1
abhanani <42608100+abhanani@users.noreply.github.com>
2020-01-30 17:27:15 +0530

    Update shutdownNode.groovy

--- vars/shutdownNode.groovy
+++ vars/shutdownNode.groovy
@@ -1,5 +1,5 @@
 def call() {
-  def get = new URL("http://stationPolicyServic-usqe3-02.qea1.tivo.com:40146/shutdown").openConnection();
+  def get = new URL("http://stationpolicyservice-usqe3-02.qea1.tivo.com:40146/shutdown").openConnection();
   get.setRequestMethod("GET")
   get.setDoOutput(true)
   def getRC = get.getResponseCode();
